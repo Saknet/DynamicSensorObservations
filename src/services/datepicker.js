13e0fcda4b6c5a007e355ca29d5582665c5a5ba3
Saknet <andreas.v.andra@gmail.com>
2023-02-23 11:18:25 +0200

    updated datepicker

--- src/services/datepicker.js
+++ src/services/datepicker.js
@@ -19,7 +19,7 @@ function initializeDatepicker () {
 
             timePicker: true,
             startDate: new Date( currentYear - 1, 0, 1 ),
-            endDate: yesterday,
+            endDate: new Date( currentYear - 1, 11, 30 ),
             minDate: new Date( currentYear - 1, 0, 1 ),
             maxDate: yesterday,
             locale: {
